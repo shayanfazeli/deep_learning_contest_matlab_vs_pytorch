8565a15e4c344a9b95eb5aab156d6293af6d831e
Shayan Fazeli <shayan.fazeli@gmail.com>
2019-05-07 10:47:36 -0700

    fetching

---
+++ extra/fetch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+axel https://www.csie.ntu.edu.tw/~cjlin/courses/optdl2019/slides/proj_efficiency_matlab/cifar10-layer3.config;
+
+axel https://www.csie.ntu.edu.tw/~cjlin/courses/optdl2019/slides/proj_efficiency_matlab/cifar10.mat;
+
+axel https://www.csie.ntu.edu.tw/~cjlin/courses/optdl2019/slides/proj_efficiency_matlab/cifar10.t.mat;
+
+axel https://www.csie.ntu.edu.tw/~cjlin/courses/optdl2019/slides/proj_efficiency_matlab/experiment.m;
+
+axel https://www.csie.ntu.edu.tw/~cjlin/courses/optdl2019/slides/proj_efficiency_matlab/mnist-layer3.config;
+
+axel https://www.csie.ntu.edu.tw/~cjlin/courses/optdl2019/slides/proj_efficiency_matlab/mnist.mat;
+
+axel mnist.t.mat;
+
